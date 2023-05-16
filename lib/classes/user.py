7cac3b79129101cc8db22b588466d11d5644f061
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-05-16 16:05:21 -0400

    changed user to users

--- lib/classes/user.py
+++ lib/classes/user.py
@@ -39,7 +39,7 @@ class User:
     @classmethod
     def create_table(cls):
         CURSOR.execute("""
-            CREATE TABLE IF NOT EXISTS user(
+            CREATE TABLE IF NOT EXISTS users(
                 id INTEGER PRIMARY KEY,
                 first_initial TEXT,
                 last_initial TEXT
@@ -52,17 +52,17 @@ class User:
     def create(cls, first_initial, last_initial):
         user = User(first_initial, last_initial)
         CURSOR.execute(f"""
-                INSERT INTO user(first_initial, last_initial)
+                INSERT INTO users (first_initial, last_initial)
                 VALUES ('{user.first_initial}', '{user.last_initial}')
             """
         )
-        new_user_id= CURSOR.execute( 'SELECT last_insert_rowid() FROM user').fetchone()[0]
+        new_user_id= CURSOR.execute( 'SELECT last_insert_rowid() FROM users').fetchone()[0]
         return cls.find_by_id(new_user_id)
 
     @classmethod
     def find_by_id(cls, id):
         if type(id) is int and id > 0:
-            sql = f"SELECT * FROM user WHERE id= {id}"
+            sql = f"SELECT * FROM users WHERE id= {id}"
             new_user= CURSOR.execute(sql).fetchone()
             if new_user :
                 return cls.db_into_instance(new_user)
@@ -74,7 +74,7 @@ class User:
     @classmethod
     def find_by_name(cls, name):
         if type is str and len(name) > 0 :
-            sql = f"SELECT * FROM user where first_initial LIKE '{name}' OR last_initial LIKE '{name}'"
+            sql = f"SELECT * FROM users where first_initial LIKE '{name}' OR last_initial LIKE '{name}'"
             users = CURSOR.execute(sql).fetchall()
             if users:
                 return [cls.db_into_instance(user)for user in users]
@@ -86,7 +86,7 @@ class User:
 
     @classmethod
     def all (cls):
-        sql ="SELECT * FROM user"
+        sql ="SELECT * FROM users"
 
         users = CURSOR.execute(sql).fetchall()
         return [cls.db_into_instance(user) for user in users]
@@ -94,3 +94,5 @@ class User:
     @classmethod
     def db_into_instance (cls, user):
         return User(user[1], user[2], user[0])
+    
+    
