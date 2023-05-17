7fb8640debea3e88094ca2e62aa7588453762a26
Halim Choi <hchoicareer@gmail.com>
2023-05-17 00:09:08 -0400

    game records user input for 60 secs

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,6 +1,10 @@
-from . import CONN, CURSOR 
-from time import sleep
+# from . import CONN, CURSOR 
+from time import sleep, time
+from threading import Thread
+import os
+import sys
 
+import ipdb
 
 class Game:
     pass
@@ -12,14 +16,49 @@ class Game:
         self.set_user(user)
 
     def start_game(self):
+        def clear_screen():
+            if os.name == "nt":
+                os.system("cls")  # For Windows
+            else:
+                os.system("clear")  # For Linux and macOS
 
-        attempt = input("Enter the word: ")
+        def user_input(timeout=60):
+            inputs = []
+            end_time = time() + timeout
+
+            def input_thread():
+                nonlocal inputs
+                nonlocal end_time
+                while time() < end_time:
+                    user_input = input()
+                    inputs.append(user_input)
+                    # clear_screen()
+                    # print(f"Seconds remaining: {max(0, int(end_time - time()))}")
+                    
+            input_thread = Thread(target=input_thread)
+            input_thread.start()
+
+            while input_thread.is_alive() and time() < end_time:
+                remaining_seconds = max(0, int(end_time - time()))
+                clear_screen()
+                # print(f"Seconds remaining: {remaining_seconds}")
+                print(f"Seconds remaining: {remaining_seconds}", end="\n", flush=True)
+                
+                sleep(1)
+
+            clear_screen()
+            if remaining_seconds == 0:
+                print("Input timeout reached.")
+                sleep(1)
+            print()
+            input_thread.join(timeout)
+
+            return inputs
         
-        for i in range(60, 0, -1):
-            print(f"Time Remaining: {i}", "\r", end="")
-            
-            # attempt = input("")
-            sleep(1)
+        user_inputs = user_input(timeout=60)
+        print("User inputs:", user_inputs)
+
+    ipdb.set_trace()
 
     def get_user(self):
         return self._user
